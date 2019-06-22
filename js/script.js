f988bd37a43f47ced63aaa3f5d54411592c23fd0
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2019-06-22 18:48:50 -0300

    add banner index

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ function mobileCloseMenu() {
   document.getElementById("menu-content").style.display = "none";
 }
 
+function bannerIndex() {
+  var date = new Date();
+  if (date.getHours() > 6 && date.getHours() < 18) {
+    var day = document.getElementById("image-home");
+    day.classList.add("dayIndex");
+  } else {
+    var night = document.getElementById("image-home");
+    night.classList.add("nightIndex");
+  }
+}
+
+window.onload = function() {
+  bannerIndex();
+}
